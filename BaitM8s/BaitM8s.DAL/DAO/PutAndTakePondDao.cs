e27456cb151bebf9cce53651c355a1549654d198
JonasHovaldt <10632099@ucn.dk>
2025-11-17 09:21:10 +0100

    Added PutAndTakePondDao class and interface to the branch

---
+++ BaitM8s/BaitM8s.DAL/DAO/PutAndTakePondDao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaitM8s.DAL.DAO
+{
+    internal class PutAndTakePondDao
+    {
+    }
+}
