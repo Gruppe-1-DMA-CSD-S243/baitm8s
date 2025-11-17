728b0e74491c7f2c938fdebbfdd70eb09863ac20
JonasHovaldt <10632099@ucn.dk>
2025-11-17 12:31:04 +0100

    Api client class and interface added to connect the WinForms to the data

---
+++ BaitM8s/BaitM8s.APIClient/Clients/PutAndTakePondApiClient.cs
@@ -0,0 +1,78 @@
+﻿using BaitM8s.APIClient.Interfaces;
+using BaitM8s.DAL.Model;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaitM8s.APIClient.Clients
+{
+    public class PutAndTakePondApiClient : IPutAndTakePondApiClient
+    {
+        private readonly RestClient _restClient;
+        private readonly string _apiBaseUri;
+        public PutAndTakePondApiClient(string apiBaseUri)
+        {
+            _apiBaseUri = apiBaseUri;
+            _restClient = new RestClient(_apiBaseUri);
+        }
+        public async Task<int> CreatePutAndTakePondAsync(PutAndTakePond pond)
+        {
+            var request = new RestRequest("PutAndTakePonds", Method.Post);
+            request.AddJsonBody(pond);
+            var response = await _restClient.ExecuteAsync<int>(request);
+            if (!response.IsSuccessful)
+            {
+                throw new Exception($"Error creating PutAndTakePond: {response.StatusCode} - {response.Content}");
+            }
+            return response.Data;
+        }
+
+        public async Task DeletePutAndTakePondAsync(int pondNumber)
+        {
+            var request = new RestRequest($"PutAndTakePonds/{pondNumber}", Method.Delete);
+            var response = await _restClient.ExecuteAsync(request);
+            if (!response.IsSuccessful)
+            {
+                throw new Exception($"Error deleting PutAndTakePond with ID {pondNumber}: {response.StatusCode} - {response.Content}");
+            }
+
+        }
+
+        public async Task<IEnumerable<PutAndTakePond>> GetAllPutAndTakePondsAsync()
+        {
+            var request = new RestRequest("PutAndTakePonds", Method.Get);
+            var response = await _restClient.ExecuteAsync<IEnumerable<PutAndTakePond>>(request);
+            if (!response.IsSuccessful)
+            {
+                throw new Exception($"Error retrieving PutAndTakePonds: {response.StatusCode} - {response.Content}");
+            }
+            return response.Data;
+        }
+
+        public async Task<PutAndTakePond> GetPutAndTakePondByIdAsync(int pondNumber)
+        {
+            var request = new RestRequest($"PutAndTakePonds/{pondNumber}", Method.Get);
+            var response = await _restClient.ExecuteAsync<PutAndTakePond>(request);
+            if (!response.IsSuccessful)
+            {
+                throw new Exception($"Error retrieving PutAndTakePond with ID {pondNumber}: {response.StatusCode} - {response.Content}");
+            }
+            return response.Data;
+        }
+
+        public async Task UpdatePutAndTakePondAsync(PutAndTakePond pond)
+        {
+            var request = new RestRequest($"PutAndTakePonds/{pond.FishingSpotNumber}", Method.Put);
+            request.AddJsonBody(pond);
+            var response = await _restClient.ExecuteAsync(request);
+            if (!response.IsSuccessful)
+            {
+                throw new Exception($"Error updating PutAndTakePond with ID {pond.FishingSpotNumber}: {response.StatusCode} - {response.Content}");
+            }
+
+        }
+    }
+}
---
+++ BaitM8s/BaitM8s.APIClient/Interfaces/IPutAndTakePondApiClient.cs
@@ -0,0 +1,18 @@
+﻿using BaitM8s.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaitM8s.APIClient.Interfaces
+{
+    public interface IPutAndTakePondApiClient
+    {
+        Task<IEnumerable<PutAndTakePond>> GetAllPutAndTakePondsAsync();
+        Task<PutAndTakePond> GetPutAndTakePondByIdAsync(int pondNumber);
+        Task<int> CreatePutAndTakePondAsync(PutAndTakePond pond);
+        Task UpdatePutAndTakePondAsync(PutAndTakePond pond);
+        Task DeletePutAndTakePondAsync(int pondNumber);
+    }
+}
