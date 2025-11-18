7a70c3daed8062df1c8465bdc8604e7ebfb818cb
hhkringel <hhkr1989@gmail.com>
2025-11-18 18:38:18 +0100

    Create Interface for AnglerAPIClient + Async
    Creation of Interface for AnglerAPIClient
    Refactoring AnglerAPIClient methods to async

---
+++ BaitM8s/BaitM8s.APIClient/Interfaces/IAnglerAPIClient.cs
@@ -0,0 +1,16 @@
+﻿using BaitM8s.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaitM8s.APIClient.Interfaces
+{
+    public interface IAnglerAPIClient
+    {
+        Task<IEnumerable<Angler>> GetAllAsync();
+        Task<Angler?> GetOneAsync(int id);
+        Task<int> CreateAsync(Angler angler);
+    }
+}
