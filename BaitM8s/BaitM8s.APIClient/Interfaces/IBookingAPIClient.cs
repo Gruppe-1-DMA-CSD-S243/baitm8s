0a678fb1dd5868a378e64f7ee6ec6c7ea4314c49
hhkringel <hhkr1989@gmail.com>
2025-11-20 13:19:42 +0100

    Creation of Interface for BookingAPIClient

---
+++ BaitM8s/BaitM8s.APIClient/Interfaces/IBookingAPIClient.cs
@@ -0,0 +1,17 @@
+﻿using BaitM8s.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaitM8s.APIClient.Interfaces
+{
+    public interface IBookingAPIClient
+    {
+        IEnumerable<Booking> GetAllAsync();
+        Booking? GetOneAsync(int id);
+        bool DeleteAsync(int id);
+        int CreateAsync(Booking booking);
+    }
+}
