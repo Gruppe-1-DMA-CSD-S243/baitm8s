2b57613a0f6836e24bfb713317804c2a64c042b4
ItsCloudxo <10503084@ucn.dk>
2025-11-20 14:44:26 +0100

    WIP

--- BaitM8s/BaitM8s.API/Controllers/FishingSpotsController.cs
+++ BaitM8s/BaitM8s.API/Controllers/FishingSpotsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BaitM8s.DAL.DTO;
+using BaitM8s.DAL.Interfaces;
+using BaitM8s.Services.Notifications.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BaitM8s.API.Controllers
 {
@@ -6,10 +9,58 @@ namespace BaitM8s.API.Controllers
     [ApiController]
     public class FishingSpotsController : Controller
     {
-        [HttpGet]
-        public IActionResult Index()
+        private readonly IFishingSpotDAO _fishingSpotDAO;
+        public FishingSpotsController(IFishingSpotDAO fishingSpotDAO)
         {
-            return View();
+            _fishingSpotDAO = fishingSpotDAO;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FishingSpotDTO>>> GetAsync()
+        {
+            try
+            {
+                return Ok(await _fishingSpotDAO.GetAllFishingSpotsAsync());
+            }
+            catch (Exception ex)
+            {
+                // This line is used for debugging.
+                return StatusCode(500, $"Error: {ex.Message}");
+
+                //return StatusCode(500, $"An error occurred trying to retrieve all fishing spots.");
+            }
+        }
+
+        [HttpGet("by-id/{id}")]
+        public async Task<ActionResult<FishingSpotDTO>> GetAsync(int id)
+        {
+            try
+            {
+                return Ok(await _fishingSpotDAO.GetFishingSpotAsync(id));
+            }
+            catch (Exception ex)
+            {
+                // This line is used for debugging.
+                return StatusCode(500, $"Error: {ex.Message}");
+
+                //return StatusCode(500, $"An error occurred trying to retrieve the fishing spot with id {id}.");
+            }
+        }
+
+        [HttpGet("by-owner/{id}")]
+        public async Task<ActionResult<IEnumerable<FishingSpotDTO>>> GetAllByOwnerAsync(int id)
+        {
+            try
+            {
+                return Ok(await _fishingSpotDAO.GetFishingSpotsByPondOwnerAsync(id));
+            }
+            catch (Exception ex)
+            {
+                // This line is used for debugging.
+                return StatusCode(500, $"Error: {ex.Message}");
+
+                //return StatusCode(500, $"An error occurred trying to retrieve the fishing spots from owner id {id}.");
+            }
         }
     }
 }
--- BaitM8s/BaitM8s.MVC/Controllers/FishingSpotController.cs
+++ BaitM8s/BaitM8s.MVC/Controllers/FishingSpotController.cs
@@ -1,12 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BaitM8s.APIClient.Interfaces;
+using BaitM8s.DAL.DTO;
+using BaitM8s.DAL.Interfaces;
+using BaitM8s.DAL.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BaitM8s.MVC.Controllers
 {
     public class FishingSpotController : Controller
     {
-        public IActionResult Index()
+        private readonly IFishingSpotDAO _fishingSpotApiClient;
+
+        public FishingSpotController(IFishingSpotDAO fishingSpotApiClient)
         {
-            return View();
+            _fishingSpotApiClient = fishingSpotApiClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OwnerOverview(int id)
+        {
+            var fishingSpots = await _fishingSpotApiClient.GetFishingSpotsByPondOwnerAsync(id);
+            return View(fishingSpots);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AllOverview()
+        {
+            var fishingSpots = await _fishingSpotApiClient.GetAllFishingSpotsAsync();
+            return View(fishingSpots);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OwnerDetails()
+        {
+            var fishingSpots = await _fishingSpotApiClient.GetAllFishingSpotsAsync();
+            return View(fishingSpots);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RegisterOwnership()
+        {
+            var fishingSpots = await _fishingSpotApiClient.GetAllFishingSpotsAsync();
+            return View(fishingSpots);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterOwnership(int id)
+        {
+            var fishingSpots = await _fishingSpotApiClient.GetAllFishingSpotsAsync();
+            return View(fishingSpots);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RemoveOwnership()
+        {
+            var fishingSpots = await _fishingSpotApiClient.GetAllFishingSpotsAsync();
+            return View(fishingSpots);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveOwnership(int id)
+        {
+            var fishingSpots = await _fishingSpotApiClient.GetAllFishingSpotsAsync();
+            return View(fishingSpots);
         }
     }
 }
