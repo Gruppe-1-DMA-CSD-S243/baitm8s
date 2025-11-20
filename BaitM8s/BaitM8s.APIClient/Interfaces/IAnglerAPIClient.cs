dd4e9d423f3ead9c2b737d88f00acacd24360d84
hhkringel <hhkr1989@gmail.com>
2025-11-20 13:18:26 +0100

    Creation of Interface for Angler APIClient

---
+++ BaitM8s/BaitM8s.APIClient/Interfaces/IAnglerAPIClient.cs
@@ -0,0 +1,17 @@
+﻿using BaitM8s.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaitM8s.APIClient.Interfaces
+{
+    public interface IAnglerAPIClient
+    {
+        IEnumerable<Angler> GetAllAsync();
+        Angler? GetOneAsync(int id);
+        bool DeleteAsync(int id);
+        int CreateAsync(Angler angler);
+    }
+}
