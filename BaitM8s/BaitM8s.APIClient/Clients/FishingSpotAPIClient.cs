89ffccd27a9b3fac05b296241be46de51f45219e
JonasHovaldt <10632099@ucn.dk>
2025-11-21 12:11:32 +0100

    Implements APIClient for FishingSpot

--- BaitM8s/BaitM8s.APIClient/Clients/FishingSpotAPIClient.cs
+++ BaitM8s/BaitM8s.APIClient/Clients/FishingSpotAPIClient.cs
@@ -1,5 +1,6 @@
 ﻿using BaitM8s.DAL.Interfaces;
 using BaitM8s.DAL.Model;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,24 +11,66 @@ namespace BaitM8s.APIClient.Clients
 {
     public class FishingSpotAPIClient : IFishingSpotDAO
     {
+        private readonly RestClient _restClient;
+        private readonly string _apiBaseUri;
+
+        public FishingSpotAPIClient(RestClient restClient, string apiBaseUri)
+        {
+            _restClient = restClient;
+            _apiBaseUri = apiBaseUri;
+        }
+
         public async Task<int> CreateFishingSpotAsync(FishingSpot fishingSpot, int pondOwnerId)
         {
-            throw new NotImplementedException();
+            var request = new RestRequest("FishingSpot", Method.Post);
+            request.AddJsonBody(fishingSpot);
+            var response = await _restClient.ExecuteAsync<int>(request);
+            if (!response.IsSuccessful)
+            {
+                throw new Exception($"Error creating FishingSpot:{response.StatusCode} - {response.Content}");
+            }
+            return response.Data;
+            
         }
 
         public async Task<bool> DeleteFishingSpotAsync(int id)
         {
-            throw new NotImplementedException();
+            var request = new RestRequest($"FishingSpot/{id}", Method.Delete);
+            var response = await _restClient.ExecuteAsync(request);
+            if (!response.IsSuccessful)
+            {
+                throw new Exception($"Error deleting FishingSpot with ID {id}: {response.StatusCode} - {response.Content}");
+            }
+            return true;
         }
 
         public async Task<IEnumerable<FishingSpot>> GetAllFishingSpotsAsync()
         {
-            throw new NotImplementedException();
+            var request = new RestRequest("FishingSpot", Method.Get);
+            var response = await _restClient.ExecuteAsync<IEnumerable<FishingSpot>>(request);
+            if (response == null)
+            {
+                throw new Exception("Response is null");
+            }
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Data;
+            }
+            else
+            {
+                throw new Exception($"Error retrieving FishingSpot: {response.StatusCode} - {response.Content}");
+            }
         }
 
         public async Task<FishingSpot?> GetFishingSpotAsync(int id)
         {
-            throw new NotImplementedException();
+            var request = new RestRequest($"FishingSpot/{id}", Method.Get);
+            var response = await _restClient.ExecuteAsync<FishingSpot>(request);
+            if (!response.IsSuccessful)
+            {
+                throw new Exception($"Error retrieving FishingSpot with ID {id}: {response.StatusCode} - {response.Content}");
+            }
+            return response.Data;
         }
 
         public async Task<IEnumerable<FishingSpot>> GetFishingSpotsByPondOwnerAsync(int id)
@@ -45,9 +88,16 @@ namespace BaitM8s.APIClient.Clients
             throw new NotImplementedException();
         }
 
-        public Task<int> UpdateFishingSpotAsync(int id, FishingSpot fishingSpot)
+        public async Task<int> UpdateFishingSpotAsync(int id, FishingSpot fishingSpot)
         {
-            throw new NotImplementedException();
+            var request = new RestRequest($"FishingSpot/{id}", Method.Put);
+            request.AddJsonBody(fishingSpot);
+            var response = await _restClient.ExecuteAsync<int>(request);
+            if (!response.IsSuccessful)
+            {
+                throw new Exception($"Error updating FishingSpot with ID {id}: {response.StatusCode} - {response.Content}");
+            }
+            return response.Data;
         }
     }
 }
