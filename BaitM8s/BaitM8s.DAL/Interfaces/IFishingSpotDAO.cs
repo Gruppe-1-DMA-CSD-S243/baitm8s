c5b182a827a63699258c7915549ca254fa6d1061
ItsCloudxo <10503084@ucn.dk>
2025-11-24 09:19:02 +0100

    Reapply "Debug"
    This reverts commit b29f250d04d64818b751be4b40e9a2475d346e1d.

--- BaitM8s/BaitM8s.DAL/Interfaces/IFishingSpotDAO.cs
+++ BaitM8s/BaitM8s.DAL/Interfaces/IFishingSpotDAO.cs
@@ -1,4 +1,4 @@
-﻿using BaitM8s.DAL.Model;
+﻿using BaitM8s.DAL.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +9,13 @@ namespace BaitM8s.DAL.Interfaces
 {
     public interface IFishingSpotDAO
     {
-        Task<FishingSpot?> GetFishingSpotAsync(int id);
-        Task<IEnumerable<FishingSpot>> GetAllFishingSpotsAsync();
-        Task<IEnumerable<FishingSpot>> GetFishingSpotsByPondOwnerAsync(int id);
-        Task<FishingSpot> RemoveOwnershipOnFishingSpotAsync(int id);
+        Task<FishingSpotDTO?> GetFishingSpotAsync(int id);
+        Task<IEnumerable<FishingSpotDTO>> GetAllFishingSpotsAsync();
+        Task<IEnumerable<FishingSpotDTO>> GetFishingSpotsByPondOwnerAsync(int id);
+        Task<FishingSpotDTO> RemoveOwnershipOnFishingSpotAsync(int id);
         Task<bool> DeleteFishingSpotAsync(int id);
-        Task<FishingSpot> RegisterFishingSpotAsync(int id);
-        Task<int> CreateFishingSpotAsync(FishingSpot fishingSpot);
+        Task<FishingSpotDTO> RegisterFishingSpotAsync(int id);
+        Task<int> CreateFishingSpotAsync(FishingSpotDTO fishingSpot);
 
     }
 }
