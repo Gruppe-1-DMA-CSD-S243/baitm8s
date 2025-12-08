97891f696ceed3751761df875c85cbd20ced8dd5
Sofus Wennike <sufos400@gmail.com>
2025-12-08 09:48:11 +0100

    Add Tests for IAnglerDAO

---
+++ BaitM8s/BaitM8s.Test/Test for Angler/TestAnglerDAO.cs
@@ -0,0 +1,46 @@
+﻿using BaitM8s.DAL.DAO;
+using BaitM8s.DAL.Interfaces;
+using BaitM8s.DAL.Model;
+
+namespace BaitM8s.Test;
+
+public class TestAnglerDAO
+{
+    private readonly string _testConnectionString =
+    "Data Source=localhost;Database=BaitM8s;User ID=sa;Password=@12tf56so;Trust Server Certificate=True;";
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task TestGetAnglersAsync()
+    {
+        //Arrange
+        IAnglerDAO DAO = new AnglerDAO(_testConnectionString);
+
+        //Act
+        IEnumerable<Angler> allAnglers = await DAO.GetAnglersAsync();
+
+        //Assert
+        Assert.NotNull(allAnglers);
+        Assert.That(allAnglers.Count() > 0);
+    }
+
+    [Test]
+    public async Task TestGetAnglerAsync()
+    {
+        //Arrange
+        IAnglerDAO DAO = new AnglerDAO(_testConnectionString);
+        IEnumerable<Angler> allAnglers = await DAO.GetAnglersAsync();
+        int id = allAnglers.Min(angler => angler.Id);
+
+        //Act
+        Angler foundAngler = await DAO.GetAnglerAsync(id);
+
+        //Assert
+        Assert.NotNull(foundAngler);
+        Assert.True(foundAngler.Id == id);
+    }
+}
